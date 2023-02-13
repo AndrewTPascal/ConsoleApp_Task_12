7401c4d354bf2e4789e832dee508d2551c531f04
AndrewTPascal <andrvotkinski@yandex.ru>
2023-02-14 00:22:41 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp_Task_12.1.5/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp_Task_12._1._5
+{
+    class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+        public User(string login, string name, bool isPremium)
+        {
+            Login = login;
+            Name = name;
+            IsPremium = isPremium;
+
+            Console.WriteLine("Здравствуйте, " + Name);
+            if (!IsPremium) ShowAds();
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           User user1 = new User ("Vovka", "Владимир", true);
+           User user2 = new User("Alex", "Алексей", false);
+        }
+    }
+}
